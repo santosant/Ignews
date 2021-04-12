f7eece7c9df524a4f453d9b5d73172a4186df54d
santosant <hiroto.anderson@gmail.com>
2021-04-11 21:53:42 -0300

    stripe webhook config added

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,19 +1,36 @@
-import { session, signIn, useSession } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton( {priceId }: SubscribeButtonProps) {
+export function SubscribeButton( { priceId }: SubscribeButtonProps) {
   const session = useSession();
   
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if (!session) {
       signIn('github')
       return;
     }
-  }
+
+    try {
+      const response = await api.post('/checkoutSession')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+      console.log(sessionId)
+
+      await stripe.redirectToCheckout({ sessionId: sessionId })
+
+    } catch (err) {
+      alert(err.message);
+    }  
+  } 
+
   return (
     <button
       type="button"
